cbb02b59704ceea230783fd4f3567891c66a9f51
dodziraynard <45526474+dodziraynard@users.noreply.github.com>
2021-03-12 18:28:27 +0000

    Add school info update page

--- school/urls.py
+++ school/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "school"
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
+    path("school", views.school, name="school"),
 ]
--- school/views.py
+++ school/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from school.models import School
+
 
 def dashboard(request):
     template_name = "school/dashboard.html"
@@ -6,3 +8,14 @@ def dashboard(request):
 
     }
     return render(request, template_name, context)
+
+def school(request):
+    template_name = "school/school.html"
+    if request.method == "POST":
+        post_data = request.POST.copy()
+        post_data.pop("csrfmiddlewaretoken")
+        school_data = {k:v for k, v in post_data.items()}
+        School.objects.all().update(**school_data)
+        return redirect("school:school")
+      
+    return render(request, template_name)
