081f59d9ba63bd13649e2e183d3c88261c355f41
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2022-06-28 11:13:03 +0000

    Implement login for anseco demo

--- curie/settings/test.py
+++ curie/settings/test.py
@@ -2,17 +2,15 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = 'tdmavu&t#o632z(si4s&^9hiw$0v1a7i%(hm6hdjf^@jb&g1d!q'
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
-
 
 # Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
