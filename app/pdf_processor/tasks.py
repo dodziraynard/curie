043ec96f1f6360818b50250e097d51cf8e5d1df7
Raynard Dodzi Helegah <70073581+hrdpayhub@users.noreply.github.com>
2023-06-16 20:06:37 +0000

    send bill via sms

--- app/pdf_processor/tasks.py
+++ app/pdf_processor/tasks.py
@@ -213,9 +213,9 @@ def generate_and_send_bill_via_sms(session_id, student_ids=None, base_url="http:
             sms_lines = [
                 "Dear Parent,", f"Bill summary for your ward, {student.user.get_name()}, {invoice.session.name}:",
             ]
-            for item in invoice.invoice_items.filter(deleted=False):
-                sms_lines.append(
-                    f"{item.name} : GHC{item.amount} ({item.type}).")
+            # for item in invoice.invoice_items.filter(deleted=False):
+            #     sms_lines.append(
+            #         f"{item.name} : GHC{item.amount} ({item.type}).")
 
             arears = round(
                 total - float(student.user.account.amount_payable), 2)
